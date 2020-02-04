df4576900d0db23a4ed57fd81806994af72c78c9
Jacob Lambert <jacob.lambert@gmail.com>
2020-02-03 22:20:33 -0500

    Add behavior setter

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,18 +6,21 @@ import "./App.css";
 
 const App: React.FC = () => {
   // Presentational state, doesn't cross into wasm
-  const [cellSize, setCellSize] = useState(3);
+  const [cellSize, setCellSize] = useState(1);
   const [colors, setColors] =
     useState(["#26547C", "#EF476F", "#FFD166", "#06D6A0", "#FCFCFC"]);
   const [running, setRunning] = useState(true);
 
   // State that is moved into wasm
-  const [height, setHeight] = useState(256);
-  const [width, setWidth] = useState(256);
+  const [height, setHeight] = useState(512);
+  const [width, setWidth] = useState(512);
   const [behaviors, setBehaviors] = useState("LLRR");
 
   return <div className="App">
-    <ControlPanel running={running} setRunning={setRunning}/>
+    <ControlPanel running={running} 
+                  setRunning={setRunning}
+                  behaviors={behaviors}
+                  setBehaviors={setBehaviors}/>
     <Canvas running={running}
             height={height}
             width={width}
--- web/src/ControlPanel.tsx
+++ web/src/ControlPanel.tsx
@@ -4,14 +4,23 @@ import './ControlPanel.css';
 
 type ControlPanelProps = {
   running: boolean,
-  setRunning: (n: any) => void,
+  setRunning: (b: boolean) => void,
+  behaviors: string,
+  setBehaviors: (s: string) => void,
 }
 
-const ControlPanel: React.FC<ControlPanelProps> = ({running, setRunning}) => {
+const ControlPanel: React.FC<ControlPanelProps> = (
+  {running, setRunning, behaviors, setBehaviors}) => {
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setBehaviors(event.target.value);
+  }
+
   return <div className="ControlPanel">
          <button onClick={() => setRunning(!running)}>
           {running ? "⏸" : "▶️"}
          </button>
+         <input type="text" value={behaviors} onChange={handleChange} />
     </div>
 };
 
